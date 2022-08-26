c53c4c0fbd3f60301fc32c6fb5620a2b103aacdc
Harrison E. Tanojo <harrisoneliezer@gmail.com>
2022-08-26 20:30:32 +0700

    push firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:indonesia_guide/Screen/WelcomePage.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
